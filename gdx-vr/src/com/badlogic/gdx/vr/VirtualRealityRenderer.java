a31e166da58aac04f4e48d8c475f985e6a984dbf
Daniel Holderbaum <danielholderbaum@gmail.com>
2014-09-11 07:49:04 +0200

    Corrected the dependencies

--- gdx-vr/src/com/badlogic/gdx/vr/VirtualRealityRenderer.java
+++ gdx-vr/src/com/badlogic/gdx/vr/VirtualRealityRenderer.java
@@ -12,4 +12,31 @@ package com.badlogic.gdx.vr;
  */
 public class VirtualRealityRenderer {
 
+	/**
+	 * Enables or disables VR rendering mode.
+	 * 
+	 * Controls stereo rendering and distortion correction. Enabled by default.
+	 * Changes will be effective from the first frame after this call.
+	 * 
+	 * If disabled, no interpupillary distance will be applied to the eye
+	 * transformations and automatic distortion correction will not take place.
+	 * Changes will be applied to the next frames being drawn.
+	 * 
+	 * See the documentation of the Renderer and StereoRenderer interfaces for
+	 * details on how they are affected by VR mode.
+	 */
+	public void setVRMode(boolean enabled) {
+
+	}
+
+	/**
+	 * Sets whether distortion correction is enabled.
+	 * 
+	 * Enabled by default. Changes will be effective from the first frame after
+	 * this call.
+	 */
+	public void setDistortionCorrectionEnabled(boolean enabled) {
+
+	}
+	
 }
